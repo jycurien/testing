49f2e04f08d7d9c189e4e54ea9f9322bcfd187a8
Jean-Yves Curien <jy.curien@laposte.net>
2023-02-24 17:29:48 +0100

    Get size description

---
+++ tests/Entity/DinosaurTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Tests\Unit\Entity;
+
+use App\Entity\Dinosaur;
+use PHPUnit\Framework\TestCase;
+
+class DinosaurTest extends TestCase
+{
+    public function testCanGetAndSetData(): void
+    {
+        $dino = new Dinosaur(
+            name: 'Big Eaty',
+            genus: 'Tyrannosaurus',
+            length: 15,
+            enclosure: 'Paddock A',
+        );
+        self::assertSame('Big Eaty', $dino->getName());
+        self::assertSame('Tyrannosaurus', $dino->getGenus());
+        self::assertSame(15, $dino->getLength());
+        self::assertSame('Paddock A', $dino->getEnclosure());
+    }
+
+    /**
+     * @dataProvider sizeDescriptionProvider
+     */
+    public function testGetSizeDescription(int $length, string $expectedSize): void
+    {
+        $dino = new Dinosaur(name: 'Big Eaty', length: $length);
+        
+        self::assertSame($expectedSize, $dino->getSizeDescription());
+    }
+
+    public function sizeDescriptionProvider(): \Generator
+    {
+        yield '10 Meter Large Dino' => [10, 'Large'];
+        yield '5 Meter Medium Dino' => [5, 'Medium'];
+        yield '4 Meter Small Dino' => [4, 'Small'];
+    }
+}
